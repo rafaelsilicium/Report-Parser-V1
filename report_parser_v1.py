08bb8fdd4cdb097a3c82c627bc411a1c39bf2fca
Rafael R. Schaeffer <rafschaeffer@gmail.com>
2026-05-14 16:35:05 -0300

    Edge Cases e melhoria na leitura dos arquivos Essa versão realiza melhorias na leitura dos arquivos .txt e trata os seguintes edge cases:
    - Arquivos .txt vazios: através da linha 28, verifica-se a existência de conteúdo dentro do arquivo .txt antes de realizar sua análise, o ignorando caso esteja vazio
    
    - Campos faltando: através das linhas 36-51 verifica se há cada um dos headers dentro dos conteúdos dos arquivos .txt, os atualizando adicionando o campo ausente com a informação "NÃO INFORMADO" caso esteja incompleto.
    
    Teste: criando arquivos .txt vazios e removendo campos dos resumos usados para teste, avaliando a criação do arquivo .csv indicando a ausência dos campos e ignorando os arquivos vazios.

--- report_parser_v1.py
+++ report_parser_v1.py
@@ -11,49 +11,82 @@ caminho_arquivo_csv = 'C:/Users/rafsc/OneDrive/Área de Trabalho/Relatorios txt/
 #Indicação do título das separações dentro dos arquivos .txt
 headers = ['NOME DO PROJETO', 'DATA', 'STATUS', 'RESUMO']
 
-
 #Função para ler os arquivos .txt e juntar os conteúdos em uma única variável
-def leitura_arquivos_txt(arquivo_resumo):
+def leitura_arquivos_txt():
     
     dados_juntos = [] #Variável que receberá o conteúdo dos arquivos .txt unidos
+    print("\nIniciando a leitura dos arquivos .txt\n")
     
     #Analisa todos os arquivos da pasta indicada
     for arquivo_resumo in caminho_pasta.glob('*.txt'):
         
         dados_separados = {} #Variável que recebe os conteúdos do arquivo atual
-        
-        conteudo_resumo = arquivo_resumo.read_text(encoding = 'utf-8').splitlines()
-        print ('Arquivo',arquivo_resumo.name,': \n')
+        conteudo_resumo = arquivo_resumo.read_text(encoding = 'utf-8-sig').splitlines()
+        print (f"\nLendo o arquivo {arquivo_resumo.name}")
         
         #Separa na variável os cabeçalhos dos conteúdos, apresentando-os aos pares
-        for line in conteudo_resumo:
-            if ':' in line:
-                head, contend = line.split(':')
-                dados_separados[head.strip()] = line.strip(head).strip(': ')
+        if arquivo_resumo.stat().st_size != 0:
+            
+            #variável para indicação de atualização do arquivo .txt no caso de haver algum campo faltando 
+            atualiza_leitura_txt = False
+            #Remove espaços e tabulações de cada uma das linhas do conteudo do arquivo .txt 
+            conteudo_resumo_strip = [linha.strip() for linha in conteudo_resumo]
+            
+            #Procura cada uma das palavras-chave (headers) no arquivo .txt atual, indicando se falta algum
+            for h in headers:    
+                h_strip = h.strip()
+                header_presente = any(h_strip in linha for linha in conteudo_resumo_strip)
+                
+                #caso falte algum header, apresenta no terminal o header faltando e modifica o arquivo .txt
+                #adicionando "NÃO INFORMADO" ao campo ausente
+                if not (header_presente):
+                    print(f"Header {h} faltando!\n")
+                    atualiza_leitura_txt = True
+                
+                    with open(arquivo_resumo,'a', encoding='utf-8-sig') as txt_edit:
+                        txt_edit.write(f"\n{h}: NÃO INFORMADO")
+            
+            #Caso o arquivo .txt tenha sido atualizado pela busca dos headers, refaz a leitura do conteúdo
+            if atualiza_leitura_txt:
+                conteudo_resumo = arquivo_resumo.read_text(encoding = 'utf-8-sig').splitlines()    
+           
+           #Separa as palavras chave dos conteúdos, a partir do caractere ":"
+            for line in conteudo_resumo:
+                if ':' in line:
+                    head, contend = line.split(':')
+                    dados_separados[head.strip()] = line.strip(head).strip(': ')
+
+            #Adiciona na variável geral o conteúdo da variável parcial
+            dados_juntos.append(dados_separados)      
         
-        #Print do conteúdo do arquivo .txt atual
-        print(dados_separados, "\n\n")
-        #Adiciona na variável geral o conteúdo da variável parcial
-        dados_juntos.append(dados_separados)      
+        else:
+            print("Arquivo ignorado! .txt vazio!" )
         
     return dados_juntos #Retorna a variável com todos os conteúdos unidos
 
 
 def main():
 
+    print("\n--------Report Parser V1--------\n")
     #Abre o arquivo .csv para escrita
     with open(caminho_arquivo_csv, 'a', newline='', encoding = 'utf-8-sig') as planilha_csv:
         
-        #Caso o arquivo esteja vazio, escreve na primeira linha o título de cada coluna
+        #Caso o arquivo esteja vazio, escreve na primeira linha o título 
+        # de cada coluna
         if os.path.getsize(caminho_arquivo_csv) == 0:
+            print("Arquivo .csv criado!")
             adicionar_headers = csv.writer(planilha_csv, delimiter = ';')
             adicionar_headers.writerow(headers)
+            print("Cabeçalho do arquivo .csv adicionado!")
         
-        #Escreve no .csv o conteúdo recebido da análise dos arquivos .txt    
+        #Escreve no .csv o conteúdo recebido da análise dos arquivos .txt 
+        #retornada pela funcao leitura_arquivos_txt   
         adicionar_linha = csv.DictWriter(planilha_csv, fieldnames=headers, delimiter=';')
-        adicionar_linha.writerows(leitura_arquivos_txt(caminho_pasta))
+        adicionar_linha.writerows(leitura_arquivos_txt())
+        print("\nEdição do arquivo .csv finalizada!")
         
-    
+    print("Fim da execução...")
+        
 
 if __name__ == "__main__":
     main()
