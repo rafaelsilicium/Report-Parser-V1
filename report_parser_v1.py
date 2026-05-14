ab8a048225c8fdd69080753b8e01a3736af1f433
Rafael R. Schaeffer <rafschaeffer@gmail.com>
2026-05-14 11:40:48 -0300

    Implementa análise de relatórios de texto para CSV

--- report_parser_v1.py
+++ report_parser_v1.py
@@ -1,31 +1,60 @@
 import os
 from pathlib import Path
+import csv
 
-def leitura_arquivos_txt(arquivo_resumo):
-    
-    for arquivo_resumo in caminho_pasta.glob('*.txt'):
-        
-        dados_separados = {}
-        
-        conteudo_resumo = arquivo_resumo.read_text(encoding = 'utf-8').splitlines()
-        print ('Arquivo {arquivo_resumo.name}: \n')
-        
-        for line in conteudo_resumo:
-            if ':' in line:
-                head, contend = line.split(':', 2)
-                print(head, ", ", contend)
-                dados_separados[head.strip()] = line.strip()
-        
-        print(dados_separados["NOME DO PROJETO"])
-        print(dados_separados["DATA"])
-        print(dados_separados["STATUS"])
-        print(dados_separados["RESUMO"])
-        print("\n\n")
-    
-    return 1
-
-
+#Configuração do caminho da pasta com os relatórios em .txt
 caminho_pasta = Path("C:/Users/rafsc/OneDrive/Área de Trabalho/Relatorios txt")
 
-ret = leitura_arquivos_txt(caminho_pasta)
+#Configuração do caminho e nome do arquivo .csv a ser criado
+caminho_arquivo_csv = 'C:/Users/rafsc/OneDrive/Área de Trabalho/Relatorios txt/projetos.csv'
+
+#Indicação do título das separações dentro dos arquivos .txt
+headers = ['NOME DO PROJETO', 'DATA', 'STATUS', 'RESUMO']
+
+
+#Função para ler os arquivos .txt e juntar os conteúdos em uma única variável
+def leitura_arquivos_txt(arquivo_resumo):
+    
+    dados_juntos = [] #Variável que receberá o conteúdo dos arquivos .txt unidos
+    
+    #Analisa todos os arquivos da pasta indicada
+    for arquivo_resumo in caminho_pasta.glob('*.txt'):
+        
+        dados_separados = {} #Variável que recebe os conteúdos do arquivo atual
+        
+        conteudo_resumo = arquivo_resumo.read_text(encoding = 'utf-8').splitlines()
+        print ('Arquivo',arquivo_resumo.name,': \n')
+        
+        #Separa na variável os cabeçalhos dos conteúdos, apresentando-os aos pares
+        for line in conteudo_resumo:
+            if ':' in line:
+                head, contend = line.split(':')
+                dados_separados[head.strip()] = line.strip(head).strip(': ')
+        
+        #Print do conteúdo do arquivo .txt atual
+        print(dados_separados, "\n\n")
+        #Adiciona na variável geral o conteúdo da variável parcial
+        dados_juntos.append(dados_separados)      
+        
+    return dados_juntos #Retorna a variável com todos os conteúdos unidos
+
+
+def main():
+
+    #Abre o arquivo .csv para escrita
+    with open(caminho_arquivo_csv, 'a', newline='', encoding = 'utf-8-sig') as planilha_csv:
+        
+        #Caso o arquivo esteja vazio, escreve na primeira linha o título de cada coluna
+        if os.path.getsize(caminho_arquivo_csv) == 0:
+            adicionar_headers = csv.writer(planilha_csv, delimiter = ';')
+            adicionar_headers.writerow(headers)
+        
+        #Escreve no .csv o conteúdo recebido da análise dos arquivos .txt    
+        adicionar_linha = csv.DictWriter(planilha_csv, fieldnames=headers, delimiter=';')
+        adicionar_linha.writerows(leitura_arquivos_txt(caminho_pasta))
+        
+    
+
+if __name__ == "__main__":
+    main()
     
